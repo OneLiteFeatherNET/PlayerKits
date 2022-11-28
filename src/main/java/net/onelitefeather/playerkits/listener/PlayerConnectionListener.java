f4a08cbd6b88a97e5c41990a8c518107339df7c3
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-11-28 22:25:23 +0100

    Rename PlayerKitManager to PlayerKitService

--- src/main/java/net/onelitefeather/playerkits/listener/PlayerConnectionListener.java
+++ src/main/java/net/onelitefeather/playerkits/listener/PlayerConnectionListener.java
@@ -1,14 +1,14 @@
 package net.onelitefeather.playerkits.listener;
 
 import net.onelitefeather.playerkits.PlayerKitsPlugin;
-import net.onelitefeather.playerkits.kit.PlayerKitManager;
+import net.onelitefeather.playerkits.service.PlayerKitService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
-public record PlayerConnectionListener(@NotNull PlayerKitsPlugin plugin, @NotNull PlayerKitManager kitManager) implements Listener {
+public record PlayerConnectionListener(@NotNull PlayerKitsPlugin plugin, @NotNull PlayerKitService kitManager) implements Listener {
 
     @EventHandler
     private void handlePlayerJoin(PlayerJoinEvent event) {
