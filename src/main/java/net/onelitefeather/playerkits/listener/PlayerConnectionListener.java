6ef9e411e94bd60ed24cdca16f472e91ccccaaeb
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-12-16 06:49:53 +0100

    Add first join kit

--- src/main/java/net/onelitefeather/playerkits/listener/PlayerConnectionListener.java
+++ src/main/java/net/onelitefeather/playerkits/listener/PlayerConnectionListener.java
@@ -1,6 +1,9 @@
 package net.onelitefeather.playerkits.listener;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.onelitefeather.playerkits.PlayerKitsPlugin;
+import net.onelitefeather.playerkits.kit.KitClaimResult;
 import net.onelitefeather.playerkits.service.PlayerKitService;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,14 +11,26 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
-public record PlayerConnectionListener(@NotNull PlayerKitsPlugin plugin, @NotNull PlayerKitService kitManager) implements Listener {
+public record PlayerConnectionListener(@NotNull PlayerKitsPlugin plugin,
+                                       @NotNull PlayerKitService kitService) implements Listener {
 
     @EventHandler
     private void handlePlayerJoin(PlayerJoinEvent event) {
 
         Player player = event.getPlayer();
-        if (this.plugin.isSpecialPlayer(player)) {
-            this.kitManager.grantSpecialKit(player);
+
+        var playerKit = this.kitService.getPlayerKit(PlayerKitService.DRACONIA_KIT_NAME);
+        if (playerKit != null) {
+            if(!this.plugin.isSpecialPlayer(player)) return;
+            this.plugin.removeSpecialPlayer(player);
+            player.sendMessage(MiniMessage.miniMessage().deserialize(this.plugin.i18n().getMessage("kit.grant.special",
+                    this.plugin.i18n().getPrefix(),
+                    LegacyComponentSerializer.legacyAmpersand().serialize(player.displayName()))));
+        } else {
+            var kit = this.kitService.getFirstJoinKit();
+            if (kit != null && this.plugin.getClaimedKitService().canClaim(player.getUniqueId(), kit.getName()) == KitClaimResult.SUCCESS) {
+                this.kitService.handleGrantKit(player, player, kit, true);
+            }
         }
     }
 }
