5009578a806a77539d1b07f0500d686a9abd22d7
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-12-16 06:53:34 +0100

    Add constant NO_COOLDOWN and add new method

--- src/main/java/net/onelitefeather/playerkits/util/TimeUtil.java
+++ src/main/java/net/onelitefeather/playerkits/util/TimeUtil.java
@@ -1,17 +1,24 @@
 package net.onelitefeather.playerkits.util;
 
+import net.onelitefeather.playerkits.kit.PlayerKit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
 
 public final class TimeUtil {
 
+    public static final long NO_COOLDOWN = -1;
+
     private TimeUtil() {
         throw new IllegalStateException("Utility class");
     }
 
+    public static long getCooldownTime(@NotNull PlayerKit playerKit) {
+        return getCooldownTime(playerKit.getTimeUnit(), playerKit.getCooldownTime());
+    }
 
     public static long getCooldownTime(@NotNull TimeUnit timeUnit, long time) {
+        if (time == NO_COOLDOWN) return time;
         return System.currentTimeMillis() + switch (timeUnit) {
             case DAYS -> (1000 * 60 * 60 * 24) * time;
             case HOURS -> (1000 * 60 * 60) * time;
