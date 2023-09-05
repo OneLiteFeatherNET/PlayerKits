0935c54722fdedd382bbdcbb38bc8d18b4e61faf
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2023-09-05 19:46:37 +0200

    Clean up code

--- src/main/java/net/onelitefeather/playerkits/util/TimeUtil.java
+++ src/main/java/net/onelitefeather/playerkits/util/TimeUtil.java
@@ -1,31 +1,66 @@
 package net.onelitefeather.playerkits.util;
 
-import net.onelitefeather.playerkits.kit.PlayerKit;
+import net.onelitefeather.playerkits.service.ClaimedKitService;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.TimeUnit;
-
 public final class TimeUtil {
-
-    public static final long NO_COOLDOWN = -1;
 
     private TimeUtil() {
         throw new IllegalStateException("Utility class");
     }
 
-    public static long getCooldownTime(@NotNull PlayerKit playerKit) {
-        return getCooldownTime(playerKit.getTimeUnit(), playerKit.getCooldownTime());
+    /**
+     * @param end the end time
+     * @return A Human read-able timestamp.
+     */
+    @NotNull
+    public static String getRemainingTime(long end) {
+
+        long diff = ceilDiv(end - System.currentTimeMillis(), 1000L);
+        String remainingTime;
+
+        if (diff > 60 * 60 * 24) {
+
+            remainingTime = "<lang:remaining-time-days:'%s':'%s':'%s':'%s'>".formatted(
+                    diff / 60 / 60 / 24,
+                    diff / 60 / 60 % 24,
+                    diff / 60 % 60,
+                    diff % 60);
+
+        } else if (diff > 60 * 60) {
+            remainingTime = "<lang:remaining-time-days:'%s':'%s':'%s'>".formatted(diff / 60 / 60, diff / 60 % 60, diff % 60);
+        } else if (diff > 60) {
+            remainingTime = "<lang:remaining-time-days:'%s':'%s'>".formatted(diff / 60, diff % 60);
+        } else {
+            remainingTime = "<lang:remaining-time-days:'%s'>".formatted(diff);
+        }
+
+        return remainingTime;
     }
 
-    public static long getCooldownTime(@NotNull TimeUnit timeUnit, long time) {
-        if (time == NO_COOLDOWN) return time;
-        return System.currentTimeMillis() + switch (timeUnit) {
-            case DAYS -> (1000 * 60 * 60 * 24) * time;
-            case HOURS -> (1000 * 60 * 60) * time;
-            case MINUTES -> (1000 * 60) * time;
-            case SECONDS -> 1000 * time;
-            default ->
-                    throw new IllegalStateException("The TimeUnit " + timeUnit.name().toLowerCase() + " is not allowed here");
+    public static long ceilDiv(long x, long y) {
+        return -Math.floorDiv(-x, y);
+    }
+
+    /**
+     * Convert a Time String to the amount of milliseconds.
+     * Credits: <a href="http://stackoverflow.com/a/8270824">...</a>
+     *
+     * @param s the time string
+     * @return the amount of milliseconds equivalent to the given string
+     */
+    public static long toMilliSec(@NotNull String s) {
+        if(s.equalsIgnoreCase("-1")) return -1L;
+        String[] sl = s.toLowerCase().split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+        long i = Long.parseLong(sl[0]);
+        return System.currentTimeMillis() + switch (sl[1]) {
+            case "s" -> i * 1000;
+            case "m" -> i * 1000 * 60;
+            case "h" -> i * 1000 * 60 * 60;
+            case "d" -> i * 1000 * 60 * 60 * 24;
+            case "w" -> i * 1000 * 60 * 60 * 24 * 7;
+            case "mo" -> i * 1000 * 60 * 60 * 24 * 30;
+            default -> ClaimedKitService.IGNORE_COOLDOWN;
         };
     }
 }
