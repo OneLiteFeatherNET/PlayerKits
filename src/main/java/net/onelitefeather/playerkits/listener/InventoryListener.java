0c6ec9502a803a5ca21e05eba17a6eb08c25bbd6
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2023-10-23 00:56:26 +0200

    Reduce code complexity

--- src/main/java/net/onelitefeather/playerkits/listener/InventoryListener.java
+++ src/main/java/net/onelitefeather/playerkits/listener/InventoryListener.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.onelitefeather.playerkits.PlayerKitsPlugin;
 import net.onelitefeather.playerkits.kit.PlayerKit;
 import net.onelitefeather.playerkits.service.PlayerKitService;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,9 +14,10 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Consumer;
+public record InventoryListener(@NotNull PlayerKitsPlugin plugin,
+                                @NotNull PlayerKitService playerKitService) implements Listener {
 
-public record InventoryListener(@NotNull PlayerKitsPlugin plugin, @NotNull PlayerKitService playerKitService) implements Listener {
+    private static final Material PLACEHOLDER_ITEM = Material.BLACK_STAINED_GLASS_PANE;
 
     @EventHandler
     public void handlePreviewInventoryClick(InventoryClickEvent event) {
@@ -37,7 +39,9 @@ public record InventoryListener(@NotNull PlayerKitsPlugin plugin, @NotNull Playe
     @EventHandler
     private void handleClick(InventoryClickEvent event) {
 
-        if (event.getWhoClicked() instanceof Player player && event.getView().getTopInventory().equals(this.playerKitService.getKitInventory())) {
+        Player player = (Player) event.getWhoClicked();
+
+        if (event.getView().getTopInventory().equals(this.playerKitService.getKitInventory())) {
 
             event.setCancelled(true);
 
@@ -47,20 +51,10 @@ public record InventoryListener(@NotNull PlayerKitsPlugin plugin, @NotNull Playe
             if (clickedInventory.equals(this.playerKitService.getKitInventory())) {
                 ItemStack currentItem = event.getCurrentItem();
                 if (currentItem == null) return;
-
-                getPlayerKit(currentItem, playerKit -> {
-                    if (playerKit != null) {
-                        handleInventoryClick(player, playerKit, event.getClick());
-                    } else {
-                        player.sendMessage(MiniMessage.miniMessage().deserialize("<lang:kit.not-found:'%s':'%s'>".formatted(this.plugin.getPluginPrefix(), getDisplayName(currentItem))));
-                    }
-                });
+                if (currentItem.getType() == PLACEHOLDER_ITEM) return;
+                performAction(player, currentItem, event.getClick());
             }
         }
-    }
-
-    private void getPlayerKit(@NotNull ItemStack currentItem, Consumer<PlayerKit> consumer) {
-        consumer.accept(this.playerKitService.getPlayerKit(currentItem.getType()));
     }
 
     @NotNull
@@ -71,6 +65,18 @@ public record InventoryListener(@NotNull PlayerKitsPlugin plugin, @NotNull Playe
 
         var displayName = itemMeta.displayName();
         return displayName != null ? displayName : itemStack.displayName();
+    }
+
+    private void performAction(Player player, ItemStack itemStack, ClickType clickType) {
+
+        var playerKit = this.playerKitService.getPlayerKit(itemStack.getType());
+        if (playerKit == null) {
+            player.sendMessage(MiniMessage.miniMessage().deserialize("<lang:kit.not-found:'%s':'%s'>".
+                    formatted(this.plugin.getPluginPrefix(), getDisplayName(itemStack))));
+            return;
+        }
+
+        handleInventoryClick(player, playerKit, clickType);
     }
 
     private void handleInventoryClick(@NotNull Player player, @NotNull PlayerKit playerKit, ClickType clickType) {
