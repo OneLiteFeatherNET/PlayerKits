3e7dcff0dc9fadf1504d632bac4e62c52ef51b1c
TheMeinerLP <p.glanz@madfix.me>
2024-11-03 12:24:22 +0000

    Update from main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,59 @@
 plugins {
-    id("java")
+    `maven-publish`
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.publishdata)
+    java
 }
 
 group = "net.onelitefeather"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
-}
+version = "0.0.1"
 
 dependencies {
-    // Paper
-    compileOnly(libs.paper)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks {
     compileJava {
-        options.release.set(17)
+        options.release.set(21)
         options.encoding = "UTF-8"
+    }
+}
+
+
+publishData {
+    addBuildData()
+    val projectId: String by project
+    val gitlabUrl: String by project
+    useGitlabReposForProject(projectId, gitlabUrl)
+    publishTask("jar") //TODO: Change
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        // configure the publication as defined previously.
+        publishData.configurePublication(this)
+        version = publishData.getVersion(false)
+    }
+
+    repositories {
+        maven {
+            credentials(HttpHeaderCredentials::class) {
+                name = "Job-Token"
+                value = System.getenv("CI_JOB_TOKEN")
+            }
+            authentication {
+                create("header", HttpHeaderAuthentication::class)
+            }
+
+
+            name = "Gitlab"
+            // Get the detected repository from the publishing data
+            url = uri(publishData.getRepository())
+        }
     }
 }
 
