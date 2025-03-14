df4583220fada8f25355e45d6d7599fd919ad9c6
Phillipp Glanz <p.glanz@madfix.me>
2025-03-14 17:16:07 +0100

    Add GitHub Actions workflows for building PRs and closing invalid PRs, and update CODEOWNERS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,43 +8,10 @@ pluginManagement {
 
 
 dependencyResolutionManagement {
-    if (System.getenv("CI") != null) {
-        repositoriesMode = RepositoriesMode.PREFER_SETTINGS
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.htl-md.schule/repository/Gitlab-Runner/")
-            maven {
-                val groupdId = 28 // Gitlab Group
-                val ciApiv4Url = System.getenv("CI_API_V4_URL")
-                url = uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name = "Job-Token"
-                    value = System.getenv("CI_JOB_TOKEN")
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
-    } else {
-        repositories {
-            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            maven("https://repo.papermc.io/repository/maven-public")
-            mavenCentral()
-            maven {
-                val groupdId = 28 // Gitlab Group
-                url = uri("https://gitlab.onelitefeather.dev/api/v4/groups/$groupdId/-/packages/maven")
-                name = "GitLab"
-                credentials(HttpHeaderCredentials::class.java) {
-                    name =  "Private-Token"
-                    value = providers.gradleProperty("gitLabPrivateToken").get()
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
+    repositories {
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://repo.papermc.io/repository/maven-public")
+        mavenCentral()
     }
 
 
